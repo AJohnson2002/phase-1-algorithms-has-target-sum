7ab52830e794471e8cf84b66bd92b00e49ed87db
Averette <averettewjohnson@gmail.com>
2023-08-20 17:25:22 -0500

    completed

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const noticedNumbers = {};
+
+  for (const number of array){
+    const i = target - number;
+    if (i in noticedNumbers) return true;
+    noticedNumbers[number] = true;
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+ This function will look at every number of the array and check to see if the target is found and if so it will return true.
+ Else it will return false.
 */
 
 /* 
-  Add your pseudocode here
+  Find if the array has the target.
+  tell true if the array has target.
+  return false if the target is not found.
 */
 
 /*
-  Add written explanation of your solution here
+  This function will find if the array has the target in it by searching each number of the array.
+  If the array does not have the desired target, than it will return false.
+  Otherwise it will return true.
 */
 
 // You can run `node index.js` to view these console logs
